59396b10c5fafd4b4352305bc1a09457abb868c7
ljmin <ljmin@DESKTOP-BS26FG0>
2020-02-09 20:40:55 +0900

    최초 커밋

---
+++ WebTest/src/com/github/test/CommitTest.java
@@ -0,0 +1,15 @@
+package com.github.test;
+
+public class CommitTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Commit Test~!!!!!");
+		System.out.println("Commit Test~!!!!!");
+		System.out.println("Commit Test~!!!!!");
+		System.out.println("Commit Test~!!!!!");
+
+	}
+
+}
